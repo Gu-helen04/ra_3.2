40a3f33a7105292a2c050597eda7a9a250e26aba
Helen <gu-helen04@mail.ru>
2024-08-07 02:04:27 +0500

    f1

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Listing from './Listing';
+import data from '../data/etsy.json'
+import '../css/main.css';
+
+function App() {
+  const list = data.map(item => { 
+    const { listing_id, url, MainImage, title, currency_code, price, quantity } = item
+    return {
+      listing_id,
+      url, 
+      MainImage, 
+      title, 
+      currency_code, 
+      price, 
+      quantity
+    }
+  })
+
+  return (
+    <div className="main-content">
+      <Listing items={list}/>
+    </div>
+  );
+}
+
+export default App;
